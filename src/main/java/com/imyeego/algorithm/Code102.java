2a6f6f62587317d22b8c8beedea55792e697773d
imyeego <imyeego@gmail.com>
2019-02-15 13:00:44 +0800

    [Offer#61 & LeetCode#103]Binary Tree Zigzag Level Order Traversal

--- src/main/java/com/imyeego/algorithm/Code102.java
+++ src/main/java/com/imyeego/algorithm/Code102.java
@@ -1,9 +1,6 @@
 package com.imyeego.algorithm;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * @authur : liuzhao
@@ -25,10 +22,15 @@ public class Code102 {
         node2.left = node3;
         node2.right = node4;
 
-        List<List<Integer>> list = levelOrder(root);
+        List<List<Integer>> list = zigzagLevelOrder(root);
         System.out.println(list);
     }
 
+    /**
+     * 层次遍历，用队列实现，from LeetCode#102 & Offer60
+     * @param root
+     * @return
+     */
     public static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
@@ -45,6 +47,44 @@ public class Code102 {
             res.add(level);
 
         }
+        return res;
+    }
+
+    /**
+     * “之”字形打印二叉树，from Offer#61 & LeetCode#103
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        boolean flag = true;
+        Deque<TreeNode> deque = new LinkedList<>();
+        Deque<TreeNode> childDeque = new LinkedList<>();
+        deque.add(root);
+
+        while (!deque.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            for (int i = deque.size(); i > 0 ; --i) {
+                TreeNode temp;
+                if (flag) {
+                    temp = deque.poll();
+                    if (temp.left != null) childDeque.offer(temp.left);
+                    if (temp.right != null) childDeque.offer(temp.right);
+                } else {
+                    temp = deque.pollLast();
+                    if (temp.right != null) childDeque.push(temp.right);
+                    if (temp.left != null) childDeque.push(temp.left);
+                }
+                level.add(temp.val);
+
+            }
+            deque.addAll(childDeque);
+            childDeque.clear();
+            flag = !flag;
+            res.add(level);
+        }
+
         return res;
     }
 }
