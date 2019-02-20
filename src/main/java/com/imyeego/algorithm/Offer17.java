33100c77b2f06aeb66effbb39fcccbdbd54e118e
imyeego <imyeego@gmail.com>
2019-02-20 15:32:27 +0800

    [Offer#17 & LeetCode#23] add merge k linked list solution

--- src/main/java/com/imyeego/algorithm/Offer17.java
+++ src/main/java/com/imyeego/algorithm/Offer17.java
@@ -1,5 +1,8 @@
 package com.imyeego.algorithm;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @Authur: liuzhao
  * @Date: 2019/2/6 0006 17:32
@@ -15,18 +18,27 @@ public class Offer17 {
         ListNode node1_ = new ListNode(-3);
         ListNode node2_ = new ListNode(13);
         ListNode node3_ = new ListNode(15);
+        ListNode node4_ = new ListNode(16);
+        ListNode node1__ = new ListNode(3);
+        ListNode node2__ = new ListNode(6);
+        ListNode node3__ = new ListNode(9);
 
         node1.next = node2;
         node2.next = node3;
         node3.next = null;
-
         node1_.next = node2_;
         node2_.next = node3_;
-        node3_.next = null;
+        node3_.next = node4_;
+        node4_.next = null;
+        node1__.next = node2__;
+        node2__.next = node3__;
+        node3__.next = null;
 
-        ListNode head = mergeTwoLists(node1, node1_);
+        ListNode[] lists = new ListNode[]{node1, node1_, node1__};
+        ListNode head = mergeKLists(lists);
+
         while (head != null){
-            System.out.println(head.val);
+            System.out.print(head.val + "->");
             head = head.next;
         }
     }
@@ -68,8 +80,30 @@ public class Offer17 {
         return p;
     }
 
+    /**
+     * from LeetCode#23
+     * @param lists
+     * @return
+     */
     public static ListNode mergeKLists(ListNode[] lists) {
-        return null;
+        if (lists == null || lists.length == 0) return null;
+        ListNode p = new ListNode(0);
+        ListNode cur = p;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (o1, o2) -> {
+            if (o1.val < o2.val) return -1;
+            else if (o1.val == o2.val) return 0;
+            else return 1;
+        });
+        for (ListNode node : lists) {
+            if (node != null) queue.add(node);
+        }
+
+        while (!queue.isEmpty()) {
+            cur.next = queue.poll();
+            cur = cur.next;
+            if (cur.next != null) queue.add(cur.next);
+        }
+        return p.next;
     }
 
 
