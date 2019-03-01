1c1bf6b076a9be680f68eb9b490151382c148f43
imyeego <imyeego@gmail.com>
2019-03-01 17:20:32 +0800

    [LeetCode#15]three sum

--- src/main/java/com/imyeego/algorithm/Code1.java
+++ src/main/java/com/imyeego/algorithm/Code1.java
@@ -1,7 +1,7 @@
 package com.imyeego.algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * @authur : liuzhao
@@ -12,11 +12,13 @@ import java.util.Map;
 public class Code1 {
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int[] res = twoSum(nums, 22);
-        for (int i = 0; i < res.length; i++) {
-            System.out.println(res[i]);
-        }
+        int[] nums = {0, 0, 0, 0};
+        List<List<Integer>> res = threeSum(nums);
+//        for (int i = 0; i < res.length; i++) {
+//            System.out.println(res[i]);
+//        }
+        System.out.println(res);
+
     }
 
     /**
@@ -58,5 +60,38 @@ public class Code1 {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * three sum from LeetCode#15
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3) return res;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++){
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = - (nums[i] + nums[j]);
+                if (map.containsKey(complement)){
+                    int[] array = {nums[i], nums[j], complement};
+                    Arrays.sort(array);
+                    List<Integer> list = arrayToList(array);
+                    if (!res.contains(list))
+                        res.add(list);
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+
+    private static List<Integer> arrayToList(int[] array) {
+        List<Integer> res = new ArrayList<>();
+        for (int num : array) {
+            res.add(num);
+        }
+        return res;
     }
 }
