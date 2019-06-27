36134b9d06e0aa909fc3a0443360eb68a7635757
imyeego <imyeego@gmail.com>
2019-06-27 17:19:03 +0800

    socket测试

---
+++ src/main/java/com/imyeego/frame/bean/Student.java
@@ -0,0 +1,68 @@
+package com.imyeego.frame.bean;
+
+import com.google.gson.annotations.Expose;
+
+public class Student {
+
+    @Expose
+    private long id;
+    @Expose
+    private String name;
+    @Expose
+    private int gender;
+    @Expose
+    private String grade;
+    @Expose
+    private String classTh;
+
+    private String isUpload;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getClassTh() {
+        return classTh;
+    }
+
+    public void setClassTh(String classTh) {
+        this.classTh = classTh;
+    }
+
+    public String getIsUpload() {
+        return isUpload;
+    }
+
+    public void setIsUpload(String isUpload) {
+        this.isUpload = isUpload;
+    }
+}
--- src/main/java/com/imyeego/frame/concurrent_modification_exception/ConcurrentTest.java
+++ src/main/java/com/imyeego/frame/concurrent_modification_exception/ConcurrentTest.java
@@ -1,20 +1,147 @@
 package com.imyeego.frame.concurrent_modification_exception;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import com.google.gson.*;
+import com.imyeego.frame.bean.Student;
+import com.imyeego.frame.generics.ICallback;
+import com.sun.istack.internal.NotNull;
 
-public class ConcurrentTest
-{
+import java.io.*;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrentTest {
+    private static volatile int i = 0;
     public static void main(String[] args) {
 //        removeByForeach();
-        removeByIterator();
+//        removeByIterator();
+//        String day = "2019-5-21";
+//        Date oldDate = null;
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+//        try {
+//            oldDate = sdf.parse(day);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        String s = sdf.format(new Date());
+//        System.out.println(new Date().after(oldDate));
+//        String time = "5:00-7:00";
+//        String[] times = time.split("-");
+//        for (String s : times) {
+//            System.out.println(s);
+//        }
+//
+//        System.out.println(time);
+
+
+
+//        String date = format("10月10日");
+//        System.out.println(date);
+
+        testSocket();
+//        testGson();
+
+//        testGenerics();
+
+    }
+
+    private static void testGenerics() {
+        ICallback<Student> list = new ICallback<Student>() {
+            @Override
+            public void onSuccess(Student student) {
+
+            }
+        };
+        Type[] types = list.getClass().getGenericInterfaces();
+        Type type = ((ParameterizedType)types[0]).getActualTypeArguments()[0];
+        Class<?> clazz = (Class<?>) type;
+        Set<Class<?>> set = new HashSet<>();
+        set.add(clazz);
+        System.out.println();
+    }
+
+    private static void testGson() {
+        String response = "[{\"result\":\"success\"}]";
+        Student student = new Student();
+        student.setId(0L);
+        student.setName("Jess");
+        student.setGender(1);
+        student.setGrade("3");
+//        student.setClassTh("2");
+//        student.setIsUpload("");
+        GsonBuilder builder = new GsonBuilder();
+        builder.excludeFieldsWithoutExposeAnnotation();
+//        builder.serializeNulls();
+        String json = builder.create().toJson(student);
+        JsonParser jsonParser = new JsonParser();
+        JsonArray jsonElements = jsonParser.parse(response).getAsJsonArray();//获取JsonArray对象
+        for (JsonElement element : jsonElements) {
+            Student bean = builder.create().fromJson(element, Student.class);
+            System.out.println(bean.getName());
+        }
+        System.out.println(json);
+    }
+
+    private static void testSocket() {
+        String str = "[{\"SN\":\"54:E1:AD:F1:8E:D5\",\"aim\":\"download_sfrz\",\"version\":\"0.1\"}]";
+        String str2 = "[{\"SN\":\"54:E1:AD:F1:8E:D5\",\"aim\":\"upload_sfrz_result\",\"version\":\"0.1\"},{\"devSN\":\"75862851220127035165\",\"zwtp\":\"\",\"rlppl\":\"0.0\",\"rlzp\":\"\",\"rzfs\":\"123\",\"rzjg\":\"2\",\"rzsj\":\"2019-06-26 09:57:15\",\"sfzzp\":\"\",\"zjhm\":\"410523198611280035\",\"zwppl\":\"0.0\"}]\n";
+        String str1 = "[{\"SN\":\"54:E1:AD:F1:8E:D5\",\"aim\":\"upload_sfrz_result\",\"version\":\"0.1\"},{\"zjhm\":\"610124198810271029\",\"rzsj\":\"2019-07-08 08:25:25\",\"rzfs\":\"12\",\"rzjg\":\"1\",\"devSN\":\"\",\"zwppl\":\"\", \"rlppl\":\"0.36\",\"rlzp\":\"\",\"zwtp\":\"\",\"sfzzp\":\"\"}]";
+        Socket client = null;
+        try {
+            client = new Socket();
+//            SocketAddress address = new InetSocketAddress("222.91.163.154", 12789);
+            SocketAddress address = new InetSocketAddress("192.168.10.101", 8888);
+            client.connect(address, 3000);
+            client.setSoTimeout(5000);
+            //构建IO
+            InputStream is = client.getInputStream();
+            OutputStream os = client.getOutputStream();
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+            //向服务器端发送一条消息
+            bw.write(str + "\r\n");
+            bw.flush();
+
+            //读取服务器返回的消息
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String mess = br.readLine();
+            System.out.println("服务器："+mess);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (client != null) {
+                try {
+                    System.out.println("连接已关闭");
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
 
-    private static void removeByForeach(){
+    private static Runnable task = () -> {
+        try {
+            Thread.sleep(5000);
+            System.out.println(++i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    private static void removeByForeach(@NotNull String s){
         List<Integer> list = new ArrayList<>(Arrays.asList(2, 3, 5, 8));
         for (Integer integer : list){
             if (integer.intValue() == 2){
@@ -29,5 +156,48 @@ public class ConcurrentTest
         list.removeIf(integer -> integer == 2);
         list.forEach(System.out::println);
 
+    }
+
+    private static String format(String date) {
+        char[] dateArray = date.toCharArray();
+        if (date.charAt(0) == '0' && date.charAt(3) == '0') {
+            char[] chars = new char[4];
+            int j = -1;
+            for (int i = 0; i < dateArray.length; i++) {
+                if (i == 0 || i == 3) {
+                    continue;
+                }
+                chars[++j] = dateArray[i];
+
+            }
+            return new String(chars);
+        }
+        if (date.charAt(0) == '0' && date.charAt(3) != '0') {
+            char[] chars = new char[5];
+            int j = -1;
+            for (int i = 0; i < dateArray.length; i++) {
+                if (i == 0) {
+                    continue;
+                }
+                chars[++j] = dateArray[i];
+
+            }
+            return new String(chars);
+        }
+
+        if (date.charAt(0) != '0' && date.charAt(3) == '0') {
+            char[] chars = new char[5];
+            int j = -1;
+            for (int i = 0; i < dateArray.length; i++) {
+                if (i == 3) {
+                    continue;
+                }
+                chars[++j] = dateArray[i];
+
+            }
+            return new String(chars);
+        }
+
+        return date;
     }
 }
