3955cd71c30ea478a543d7c775185b4dcc7ee0c4
imyeego <imyeego@gmail.com>
2019-09-25 15:36:28 +0800

    暂时注释protobuf编译生成的Java bean

--- netty-im/src/main/java/com/imyeego/server/handler/NettyServerIntializer.java
+++ netty-im/src/main/java/com/imyeego/server/handler/NettyServerIntializer.java
@@ -1,6 +1,6 @@
 package com.imyeego.server.handler;
 
-import com.imyeego.netty.protobuf.UserMsg;
+// import com.imyeego.netty.protobuf.UserMsg;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -23,7 +23,7 @@ public class NettyServerIntializer extends ChannelInitializer<SocketChannel>{
         // 解码和编码，应和客户端一致
         //传输的协议 Protobuf
         ph.addLast(new ProtobufVarint32FrameDecoder());
-        ph.addLast(new ProtobufDecoder(UserMsg.User.getDefaultInstance()));
+//        ph.addLast(new ProtobufDecoder(UserMsg.User.getDefaultInstance()));
         ph.addLast(new ProtobufVarint32LengthFieldPrepender());
         ph.addLast(new ProtobufEncoder());
 
