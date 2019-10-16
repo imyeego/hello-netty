c2a44a104b47deadacf0068afafaa8936bf1be16
imyeego <imyeego@gmail.com>
2019-10-16 22:10:11 +0800

    try-with-resource reference

--- src/main/java/com/imyeego/io/TryWith.java
+++ src/main/java/com/imyeego/io/TryWith.java
@@ -11,13 +11,28 @@ public class TryWith {
         System.out.println(b & 0xff);
         System.out.println((c & 0xff) << 8);
         System.out.println(b & 0xff + ((c & 0xff) << 8));
-
         double s = 12;
         System.out.println(s);*/
 //        System.out.println(System.getProperty("user.dir"));
-        String[] classpaths = System.getProperty("java.class.path").split(";");
+        String path = "/Users/mac/Documents/Desktop/PicFingerData";
+        File file = new File(path);
+        if (file.exists() && file.list() != null) {
+            String name = file.list()[0];
+            System.out.println(name);
+            try (FileReader fr = new FileReader(path + "/" + name);
+                 BufferedReader br = new BufferedReader(fr)){
+                 String line = br.readLine();
+                System.out.println(line);
 
-        Arrays.stream(classpaths).forEach(System.out::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+//        String[] classpaths = System.getProperty("java.class.path").split(";");
+
+//        Arrays.stream(classpaths).forEach(System.out::println);
 
 //        System.out.println(readTxt("C:\\Users\\zhongyu\\IdeaProjects\\quickjava\\src\\main\\java\\com\\imyeego\\io\\hello.txt"));
 
